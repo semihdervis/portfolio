8f3be26ed639338fb01bb9aa6c62e6b3f699a5a6
semihdervis <121356726+semihdervis@users.noreply.github.com>
2025-01-26 23:55:08 +0300

    change message

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Welcome to My Portfolio</h1>
+      <h1 className="text-4xl font-bold text-blue-500">I am testing github pages lol</h1>
     </div>
   )
 }
